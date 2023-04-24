291cd4ee6ca4c432db7f342df7b24560a16cf30c
Henrique Pureza Queiroz <henrique.pureza.queiroz@gmail.com>
2023-04-23 22:17:45 -0300

    API Python

---
+++ models.py
@@ -0,0 +1,113 @@
+# Importa as bibliotecas
+import psycopg2
+import urllib.parse as up
+import os
+from dotenv import load_dotenv
+
+# Classe Materia (model)
+class Materia:
+    # Configura o arquivo do banco de dados
+    def __init__(self) -> None:
+        """
+            Model de matéria.
+        """
+
+        load_dotenv()
+
+        up.uses_netloc.append("postgres")
+        url = up.urlparse(os.environ["DATABASE_URL"])
+
+        self.conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+        self.cur = self.conn.cursor()
+
+    def create(self, materia: str, tipo: str) -> None:
+        """
+            Cria uma nova matéria no banco de dados.
+
+            Uma matéria tem o seu nome (ex: História) e o seu tipo (se é exata, humana ou linguagem).
+
+            Parâmetros:
+                materia - Matéria a ser adicionada
+                tipo - Tipo da matéria a ser adicionada
+        """
+
+        self.cur.execute(
+            f"""
+                INSERT
+                INTO materias
+                (materia, tipo)
+                VALUES
+                ('{materia}', '{tipo}')
+            """
+        )
+
+        self.conn.commit()
+    def get(self) -> dict:
+        """
+            Obtêm todas as matérias cadastradas no banco de dados e as retorna como uma lista de tuplas, onde cada tupla é uma linha do banco.
+        """
+
+        self.cur.execute(
+            f"""
+                SELECT *
+                FROM materias
+            """
+        )
+
+        rows = self.cur.fetchall()
+        table = []
+
+        # Transforma a lista de tuplas que o sqlite3 retorna em um array de dicionários (mais semelhante ao JSON)
+        for row_data in rows:
+            row = {
+                "materia": row_data[0],
+                "tipo": row_data[1]
+            }
+
+            table.append(row)
+
+        return table
+    def update(self, materiaToUpdate: str, newMateria: str, newTipo: str) -> None:
+        """
+            Atualiza uma matéria no banco de dados.
+
+            Parâmetros:
+                materiaToUpdate - matéria existente no banco a ser atualizada
+                newMateria - Nova matéria
+                newTipo - Novo tipo de matéria
+        """
+
+        self.cur.execute(
+            f"""
+                UPDATE materias SET
+                materia = '{newMateria}',
+                tipo = '{newTipo}'
+                WHERE materia = '{materiaToUpdate}'
+            """
+        )
+
+        self.conn.commit()
+    def delete(self, materia: str) -> None:
+        """
+            Deleta uma matéria do banco de dados.
+
+            Parâmetros:
+                materia - Matéria a ser deletada do banco.
+        """
+
+        self.cur.execute(
+            f"""
+                DELETE
+                FROM materias
+                WHERE materia = '{materia}'
+            """
+        )
+
+        self.conn.commit()
---
+++ routes.py
@@ -0,0 +1,54 @@
+# Importa o Flask e o model
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from models import Materia
+
+# Instancia a aplicação Flask e o model
+app = Flask(__name__)
+CORS(app)
+materiaModel = Materia()
+
+# Rota raiz => retorna todas as matérias como um JSON
+# GET => retorna [{"materia": "Matéria", "tipo": "Tipo"}, {...}, ...]
+@app.route("/")
+def getMaterias():
+    materias = materiaModel.get()
+
+    return jsonify(materias)
+
+# Rota /create => aceita requisições POST em formato JSON; cria uma matéria.
+# POST => {"materia": "Nome da matéria", "tipo": "Tipo de matéria (se é humana, exata...)"}
+@app.route("/create", methods=["POST"])
+def createMateria():
+    req = request.get_json()
+    materia = req["materia"]
+    tipo = req["tipo"]
+
+    materiaModel.create(materia, tipo)
+
+    return jsonify({"status": "OK"})
+
+# Rota /update => aceita requisições PUT em formato JSON; atualiza uma matéria já existente.
+# PUT => {
+#          "materiaToUpdate": "Matéria existente a ser atualizada",
+#          "newMateria": "Novo nome de matéria",
+#          "newTipo": "Novo tipo de matéria"
+#        }
+@app.route("/update", methods=["PUT"])
+def updateMateria():
+    req = request.get_json()
+    materiaToUpdate = req["materiaToUpdate"]
+    newMateria = req["newMateria"]
+    newTipo = req["newTipo"]
+
+    materiaModel.update(materiaToUpdate, newMateria, newTipo)
+
+    return jsonify({"status": "OK"})
+
+# Rota /delete/(nome da matéria) => aceita requisições DELETE; deleta uma matéria do banco de dados.
+# DELETE => /delete/Matemática (deleta a matéria "Matemática")
+@app.route("/delete/<materia>", methods=["DELETE"])
+def deleteMateria(materia):
+    materiaModel.delete(materia)
+
+    return jsonify({"status": "OK"})
